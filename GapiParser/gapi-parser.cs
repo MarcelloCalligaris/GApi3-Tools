63935d9d992686ce5a744c7889371c718a23c1d5
Marcello Calligaris <marcello@marcellx.com>
2020-02-23 11:35:50 -0500

    Added code, updated readme

---
+++ GapiParser/gapi-parser.cs
@@ -0,0 +1,224 @@
+// gapi-parser.cs - parsing driver application.
+//
+// Author: Mike Kestner <mkestner@novell.com>
+//
+// Copyright (c) 2005 Novell, Inc.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of version 2 of the GNU General Public
+// License as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public
+// License along with this program; if not, write to the
+// Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+// Boston, MA 02111-1307, USA.
+
+// Modified Feb 2020 by Marcello Calligaris
+// - Use System.Diagnostics.Process.Start() instead of libc system()
+// - replaced snake_case with camelCaps
+
+using System.Threading.Tasks;
+
+namespace GApi.Parser {
+
+	using System;
+	using System.Collections;
+	using System.IO;
+	using System.Xml;
+    using System.Diagnostics;
+
+	public static class Parser  
+    {
+        public static async Task<int> Main (string[] args)
+		{
+			if (args.Length != 1) 
+            {
+				Console.WriteLine ("Usage: gapi2-parser <filename>");
+				return 0;
+			}
+
+            string perl = "C:\\Perl64\\bin\\perl.exe";
+			XmlDocument srcDoc = new XmlDocument ();
+
+			try
+            {
+                using Stream stream = File.OpenRead (args [0]);
+                srcDoc.Load (stream);
+            } catch (XmlException e) {
+				Console.WriteLine ("Couldn't open source file.");
+				Console.WriteLine (e);
+				return 1;
+			}
+
+			XmlNode root = srcDoc.DocumentElement;
+			if (root.Name != "gapi-parser-input")
+            {
+				Console.WriteLine ("Improperly formatted input file: " + args [0]);
+				return 1;
+			}
+
+			foreach (XmlNode apiNode in root.ChildNodes) 
+            {
+				if (apiNode.Name != "api")
+					continue;
+
+				string outFile = (apiNode as XmlElement).GetAttribute ("filename");
+				string preFile = outFile + ".pre";
+
+                if (File.Exists(preFile))
+                    File.Delete (preFile);
+
+				foreach (XmlNode libNode in apiNode.ChildNodes)
+                {
+					if (libNode.Name != "library")
+						continue;
+
+					string lib = (libNode as XmlElement).GetAttribute ("name");
+			
+					foreach (XmlNode nsNode in libNode.ChildNodes)
+                    {
+						if (nsNode.Name != "namespace")
+							continue;
+
+						string ns = (nsNode as XmlElement).GetAttribute ("name");
+			
+						ArrayList files = new ArrayList ();
+						Hashtable excludes = new Hashtable ();
+
+						foreach (XmlNode srcNode in nsNode.ChildNodes) 
+                        {
+							if (!(srcNode is XmlElement))
+								continue;
+
+							XmlElement elem = srcNode as XmlElement;
+
+							switch (srcNode.Name) 
+                            {
+							case "dir":
+								string dir = elem.InnerXml;
+								Console.Write ("<dir {0}> ", dir);
+								DirectoryInfo di = new DirectoryInfo (dir);
+								foreach (FileInfo file in di.GetFiles ("*.c"))
+									files.Add (dir + Path.DirectorySeparatorChar + file.Name);
+								foreach (FileInfo file in di.GetFiles ("*.h"))
+									files.Add (dir + Path.DirectorySeparatorChar + file.Name);
+								break;
+							case "file":
+								string incFile = elem.InnerXml;
+								Console.Write ("<file {0}> ", incFile);
+								files.Add (incFile);
+								break;
+							case "exclude":
+								string excFile = elem.InnerXml;
+								Console.Write ("<exclude {0}> ", excFile);
+								excludes [excFile] = 1;
+								break;
+							case "directory":
+								string dirPath = elem.GetAttribute ("path");
+								Console.Write ("<directory {0}: excluding ", dirPath);
+								Hashtable excs = new Hashtable ();
+								foreach (XmlNode excNode in srcNode.ChildNodes) {
+									if (excNode.Name != "exclude")
+										continue;
+									string excFilename = (excNode as XmlElement).InnerXml;
+									Console.Write (excFilename + " ");
+									excs [excFilename] = 1;
+								}
+								DirectoryInfo dInfo = new DirectoryInfo (dirPath);
+								foreach (FileInfo file in dInfo.GetFiles ("*.c")) {
+									if (excs.Contains (file.Name))
+										continue;
+									files.Add (dirPath + Path.DirectorySeparatorChar + file.Name);
+								}
+								foreach (FileInfo file in dInfo.GetFiles ("*.h")) {
+									if (excs.Contains (file.Name))
+										continue;
+									files.Add (dirPath + Path.DirectorySeparatorChar + file.Name);
+								}
+								Console.Write ("> ");
+								break;
+							default:
+								Console.WriteLine ("Invalid source: " + srcNode.Name);
+								break;
+							}
+						}
+
+						Console.WriteLine ();
+
+						if (files.Count == 0)
+							continue;
+
+						ArrayList realFiles = new ArrayList ();
+						foreach (string file in files)
+                        {
+							string trimFile = file.TrimEnd ();
+							if (excludes.Contains (trimFile))
+								continue;
+
+							realFiles.Add (trimFile);
+						}
+								
+						string[] fileNames = (string[]) realFiles.ToArray (typeof (string));
+						string ppArgs = string.Join (" ", fileNames);
+                        string preScript = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "gapi_pp.pl");
+						string parseScript = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "gapi2xml.pl");
+						ProcessStartInfo ppInfo = new ProcessStartInfo(perl,$"{preScript} {ppArgs}")
+                        {
+							CreateNoWindow = false,
+							UseShellExecute = false,
+							RedirectStandardOutput = true,
+							WorkingDirectory = Environment.CurrentDirectory
+
+                        }, prInfo = new ProcessStartInfo(perl,$"{parseScript} {ns} {preFile} {lib}")
+                        {
+							CreateNoWindow = false,
+							UseShellExecute = false,
+							RedirectStandardInput = true,
+							WorkingDirectory = Environment.CurrentDirectory
+                        };
+                        await ExternalStdIoPipe(ppInfo, prInfo);
+                    }
+				}
+			
+				XmlDocument final = new XmlDocument ();
+				final.Load (preFile);
+                XmlTextWriter writer = new XmlTextWriter(outFile, null)
+                {
+                    Formatting = Formatting.Indented
+                };
+                final.Save (writer);
+				File.Delete (preFile);
+			}
+
+			return 0;
+		}
+
+        private static async Task ExternalStdIoPipe(ProcessStartInfo pipeSource, ProcessStartInfo pipeDest)
+        {
+            Process preProcess = Process.Start(pipeSource);
+            Process parseProcess = Process.Start(pipeDest);
+
+			if (preProcess != null && parseProcess!=null)
+            {
+
+				while (!preProcess.HasExited)
+                {
+                    string output = await preProcess.StandardOutput.ReadToEndAsync();
+					await parseProcess.StandardInput.WriteAsync(output);
+				}
+				parseProcess.StandardInput.Flush();
+				parseProcess.StandardInput.Close();
+				parseProcess.StandardInput.Dispose();
+
+                parseProcess.WaitForExit();
+			} else throw new ApplicationException("Unable to start sub-process, Process.Start returned null");
+
+        }
+
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # GApi3-Tools
-gapi3-parser, gapi3-fixup, and gapi3-codegen for dotnet core 3.1
+
+`gapi3-parser`, `gapi3-fixup`, and `gapi3-codegen` for .NET Core 3.1
+
+## Usage
+
+### Requirements
+
+* Perl with `XML::LibXML` (for gapi3-parser to run)
+* .NET Core 3.1 SDK (for building)
+
+## About
+
+This repository is a stand alone toolset for building .NET wrappers for GLib Objects originally written in C.
+
+GapiCodegen & GapiFixup were taken from [GtkSharp/GtkSharp](https://github.com/GtkSharp/GtkSharp/tree/949ee6771c3a127a44e4d7e7976d12e2cd96fc1b/Source/Tools).
+GapiParser was taken from [mono/gtk-sharp](https://github.com/mono/gtk-sharp/tree/9dc89137ba1a5b2420523de379461928e82d0477/parser) and re-packaged for .NET core.
+
+### Modifications
+
+Minor modifications were made to GApi Parser to make it more Windows-friendly, primarily the use of `System.Diagnostics.Process` to run the perl scripts instead of using `system("gapi_pp.pl ... | gapi2xml.pl ...")`
+
+In addition, some variable and method names were modified to be more inline with .NET naming conventions. Most of the logic remains untouched.
